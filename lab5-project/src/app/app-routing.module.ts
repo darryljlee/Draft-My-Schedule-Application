a4405024e60ed96f3ed3e3a66f6bc91a99cd129b
Darryl Lee <dlee688@uwo.ca>
2020-11-28 19:34:44 -0500

    creating some routing between pages (i.e. create, manage schedule)

--- lab5-project/src/app/app-routing.module.ts
+++ lab5-project/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateScheduleComponent } from './components/create-schedule/create-schedule.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+//import { HomeComponent } from './components/home/home.component';
+import { ManageScheduleComponent } from './components/manage-schedule/manage-schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {path: 'createschedule', component: CreateScheduleComponent},
+  {path: 'manageschedule', component: ManageScheduleComponent},
+  {path:'login', component: LoginPageComponent},
+  {path: '', redirectTo:'createschedule', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+export const routingComponents = [CreateScheduleComponent, ManageScheduleComponent, LoginPageComponent]
--- lab5-project/src/app/app.module.ts
+++ lab5-project/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import { CreateScheduleComponent } from './components/create-schedule/create-schedule.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents,
+    CreateScheduleComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]

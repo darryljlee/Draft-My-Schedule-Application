285b89d21c1fedbe68609c15a0a335f62a9523cc
Darryl Lee <dlee688@uwo.ca>
2020-11-29 15:46:09 -0500

    created routing and webpage for the create account page for any user that's not logged in

--- lab5-project/src/app/app-routing.module.ts
+++ lab5-project/src/app/app-routing.module.ts
@@ -2,15 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateScheduleComponent } from './components/create-schedule/create-schedule.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
-//import { HomeComponent } from './components/home/home.component';
 import { ManageScheduleComponent } from './components/manage-schedule/manage-schedule.component';
+import { BrowseComponent } from './components/browse/browse.component';
+import { HomeComponent } from './components/home/home.component';
+import { CreateAccountComponent } from './components/create-account/create-account.component';
+
+
+
+
+
 
 const routes: Routes = [
- 
+  {path: 'home', component: HomeComponent},
   {path: 'createschedule', component: CreateScheduleComponent},
   {path: 'manageschedule', component: ManageScheduleComponent},
   {path:'login', component: LoginPageComponent},
-  {path: '', redirectTo:'createschedule', pathMatch:'full'}
+  {path:'browse', component: BrowseComponent},
+  {path:'createaccount', component:CreateAccountComponent},
+  {path: '', redirectTo:'home', pathMatch:'full'}
 ];
 
 @NgModule({
@@ -20,4 +29,4 @@ const routes: Routes = [
 export class AppRoutingModule { }
 
 
-export const routingComponents = [CreateScheduleComponent, ManageScheduleComponent, LoginPageComponent]
+export const routingComponents = [CreateScheduleComponent, ManageScheduleComponent, LoginPageComponent, BrowseComponent, HomeComponent, CreateAccountComponent]

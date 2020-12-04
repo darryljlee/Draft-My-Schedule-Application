01f1b9e2cb32098133adc9dc2cb085caba72bda6
Darryl Lee <dlee688@uwo.ca>
2020-12-04 18:54:06 -0500

    fixed a minor bug that didn't allow user to create a schedule without a description

--- lab5-project/src/app/components/create-schedule/create-schedule.component.ts
+++ lab5-project/src/app/components/create-schedule/create-schedule.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 
 //
 @Component({
@@ -10,8 +12,10 @@ import { HttpClient } from '@angular/common/http';
 export class CreateScheduleComponent implements OnInit {
   specialChars = /^[^<>:/?#@!&;]*$/;
   courses: any [ ];
+  url = "http://localhost:3000/";
 
-  constructor(private http: HttpClient) { 
+
+  constructor(private http: HttpClient, private route: Router) { 
     this.courses = []
   }
 
@@ -20,20 +24,31 @@ export class CreateScheduleComponent implements OnInit {
   createSchedule():void{
     var nameInput = (<HTMLInputElement>document.getElementById('newschedule')).value;
     var descInput = (<HTMLInputElement>document.getElementById('description')).value;
-    if(!nameInput.match(this.specialChars) || !descInput.match(this.specialChars)){
-      alert("Invalid input! Try not to enter special characters in either the name or description. These include ^[^<>:/?#@!&;]*$/") //check if any special characters are entered inside the form
-      return;
+    if(nameInput.match(this.specialChars)||descInput.match(this.specialChars)){
+      var scheduleToken = {
+        timetabletoken:localStorage.timetabletoken,
+      }
+    if(descInput==""){
+    this.http.post<any>(this.url+"api/schedules/createaschedule?name="+nameInput+"&schedToken="+localStorage.timetabletoken,"").subscribe(data=>{
+      alert (data.message);
+      //location.reload;
+    })
     }
-    
-    this.http.post<any>("http://localhost:3000/api/schedules/createaschedule?name="+nameInput+"&description="+descInput, nameInput).subscribe(data =>{ //make a post request to create a schedule when user enters Create Schedule
+    else{
+      this.http.post<any>(this.url+"api/schedules/createaschedule?name="+nameInput+"&description="+descInput+"&schedToken="+localStorage.timetabletoken,"").subscribe(data =>{ //make a post request to create a schedule when user enters Create Schedule
       alert("You've created a nice name for your schedule. The schedule name you've created is "+nameInput+" and your description is "+descInput+"") //show alert to confirm creation of schedule and the name they've selected
-      //location.reload(); //allow the page to reload with updated changes once the schedule has been created
+      location.reload(); //allow the page to reload with updated changes once the schedule has been created
     }
-  )
+      )
+    }   
+    }
+    else{
+      alert("Invalid input")
+    }
   }
   addCourseToSchedule(subject:String, courseNumber:String){ //function that's called when the [+] button is clicked next to a course
     var nameOfSchedule = (<HTMLInputElement>document.getElementById('schedulesdropdown')).value;
-    this.http.put<any>("http://localhost:3000/api/schedules/updateCourse?nameSched="+nameOfSchedule+ "&subject="+subject+"&courseNumber="+courseNumber, nameOfSchedule).subscribe(data =>{
+    this.http.put<any>(this.url+"api/schedules/updateCourse?nameSched="+nameOfSchedule+ "&subject="+subject+"&courseNumber="+courseNumber, nameOfSchedule).subscribe(data =>{
       alert("You've created a nice name for your schedule. The schedule name you've created is "+nameOfSchedule)
   }
   )
@@ -47,7 +62,7 @@ export class CreateScheduleComponent implements OnInit {
     var subjectChoice = (<HTMLInputElement>document.getElementById("Subject")).value; //create a variable that will link the subject choice to submit button  
     var numberChoice = (<HTMLInputElement>document.getElementById("numberInput")).value;
     var componentChoice = (<HTMLInputElement>document.getElementById("Component")).value;
-    var submitRequest = "http://localhost:3000/api/courses/submit?" +"Subject=" + subjectChoice + "&CourseNumber=" + numberChoice + "&Component=" + componentChoice; 
+    var submitRequest = this.url+"api/courses/submit?" +"Subject=" + subjectChoice + "&CourseNumber=" + numberChoice + "&Component=" + componentChoice; 
   
   
   this.http.get<any>(submitRequest).subscribe((data: any) => {
@@ -55,7 +70,7 @@ export class CreateScheduleComponent implements OnInit {
   
    //else if only a course number is entered, alert appears
   if(subjectChoice=="ALL SUBJECTS" && numberChoice!="" && componentChoice=="AllComponent"){
-    alert("Please don't enter just a course number only")
+    //alert("Please don't enter just a course number only")
   }
   
   //else if only a component is entered, alert appears
@@ -63,13 +78,19 @@ export class CreateScheduleComponent implements OnInit {
     alert("Please don't enter just a component only")
   }
   })}
+
+  logout(){
+    localStorage.timetabletoken="";
+  }
   
   ngOnInit(): void {
-    /*The following get request is used to get all the subject names to populate the form for Subject, and to remove duplicates when
-    receiving from the JSON file */
-    this.http.get<any>("http://localhost:3000/api/courses").subscribe(data => { 
+    
+    if(localStorage.timetabletoken=="" || localStorage.timetabletoken==undefined){
+      this.route.navigate(['/home']);
+    }
+
+    this.http.get<any>(this.url+"api/courses").subscribe(data => { 
       var getSubjects = document.getElementById("Subject"); 
-        //remove the duplicates found in the json file for all the subjects, if a duplicate is found, skip it and go to the next index
         let tempArray = [];
         for(let i=0; i<data.length; i++){  
           
@@ -91,14 +112,29 @@ export class CreateScheduleComponent implements OnInit {
     })
 
     //get request to populate the schedules dropdown to show all existing schedules 
-    this.http.get<any>("http://localhost:3000/api/schedules/getallschedules").subscribe(data =>{ 
+    this.http.get<any>(this.url+"api/schedules/getallschedules?schedToken="+localStorage.timetabletoken).subscribe(data =>{ 
       const allSchedules = document.getElementById("schedulesdropdown")
       for(let i =0; i<data.length; i++){ //
         var scheduleName = document.createElement("option");
-        var scheduleText = document.createTextNode(data[i].nameSched);
+        var scheduleText = document.createTextNode(data[i]);
         scheduleName.appendChild(scheduleText);// append to the schedules dropdown and its respective text as well
         allSchedules?.appendChild(scheduleName);
       } 
+    })
+  }
+
+  createreview(receivedSubject:String, receivedCatalogNum:String, receivedClassName:String){
+    var model = { //left side needs to match the backend
+      username: localStorage.getItem("username"),
+      courseName: receivedClassName,
+      courseSubject: receivedSubject,
+      courseID: receivedCatalogNum,
+      dateCreated: Date(),
+      theTypedReview: (<HTMLInputElement>document.getElementById("createreview")).value
+    }
+
+    this.http.put<any>(this.url+"addreview", model).subscribe(data =>{
+    alert("Your review has been added!")
     })
   }
 }
